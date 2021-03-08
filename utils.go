34ddd714e30a892cd25f1ef5a6a3e5b1bec2ae3f
Sudhakar <sudhakar.sw@gmail.com>
2021-03-08 23:47:13 +0530

    Parse time in asia/kolkata location

--- utils.go
+++ utils.go
@@ -13,6 +13,8 @@ type Time struct {
 // List of known time formats
 var ctLayouts = []string{"2006-01-02", "2006-01-02 15:04:05", "2006-01-02T15:04:05-0700"}
 
+var loc, _ = time.LoadLocation("Asia/Kolkata")
+
 // UnmarshalJSON parses JSON time string with custom time formats
 func (t *Time) UnmarshalJSON(b []byte) (err error) {
 	var pTime time.Time
@@ -25,7 +27,7 @@ func (t *Time) UnmarshalJSON(b []byte) (err error) {
 
 	// Iterate through known layouts and parse time
 	for _, l := range ctLayouts {
-		pTime, err = time.Parse(l, s)
+		pTime, err = time.ParseInLocation(l, s, loc)
 		if err == nil && !pTime.IsZero() {
 			break
 		}
@@ -40,7 +42,7 @@ func (t *Time) UnmarshalCSV(s string) (err error) {
 	var pTime time.Time
 	s = strings.TrimSpace(s)
 	for _, l := range ctLayouts {
-		pTime, err = time.Parse(l, s)
+		pTime, err = time.ParseInLocation(l, s, loc)
 		if err == nil && !pTime.IsZero() {
 			break
 		}
